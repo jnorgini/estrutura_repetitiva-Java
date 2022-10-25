160bb72c944057cee83f1c425fc52776c0906ee0
Juliana Norgini <jnorgini@gmail.com>
2022-10-25 03:37:32 -0300

    teste in out

---
+++ src/here/E2.java
@@ -0,0 +1,34 @@
+package here;
+
+import java.util.Scanner;
+
+public class E2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		int n = sc.nextInt();
+		
+		int in = 0;
+		int out = 0;
+		
+
+		for(int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+				if(x >= 10 && x <= 20) {
+					in += 1;
+				}
+				else {
+					out += 1;
+				}
+		}
+		
+		System.out.println(in + " in\n" + out + " out");
+		
+
+		sc.close();
+
+	}
+
+}
